d50b117c04b1a480a50da2695de27595cc172c4f
RukiPomidory <RukiPomidory@yandex.ru>
2019-04-26 23:43:53 +0300

    working time window, and exit from it

--- light_alarm/light_alarm.ino
+++ light_alarm/light_alarm.ino
@@ -72,6 +72,27 @@ bool settings = false;
 // Значение для сравнения (энкодер)
 bool prevClk;
 
+// Предыдущее показание кнопки энкодера
+bool prevSW;
+
+// Номер текущего окна настроек
+byte window;
+// 0 - окно не выбрано
+// 1 - настройка времени
+// 2 - настройка будильника
+
+
+// Изменяется ли сейчас время
+bool timeChanging = false;
+
+// Какой сектор времени изменяется
+byte sector;
+// 0 - день
+// 1 - месяц
+// 2 - год
+// 3 - минуты
+// 4 - часы
+
 // Счетчик кликов
 int counter;
 
@@ -113,17 +134,18 @@ void checkEncoder()
     // и в прошлый раз он был высокий
     if (!clk && prevClk)
     {
-        // Сигнал dt высокий, крутимся по часовой стрелке
-        if (dt)
+        // Сигнал dt низкий, крутимся по часовой стрелке
+        if (!dt)
         {
             counter++;
             clickClock();
         }
-        else    // А тут сигнал низкий, значит против часовой
+        else    // А тут сигнал высокий, значит против часовой
         {
             counter--;
             clickReverse();
         }
+        D("counter: ");
         D_LN(counter);
     }
     
@@ -134,31 +156,90 @@ void checkEncoder()
 // Клик по часовой стрелке
 void clickClock()
 {
-    selectOption();
+    if (settings)
+    {
+        switch(window)
+        {
+            case 0:
+                displaySettings();
+                break;
+            
+            case 1:
+                displaySetTime();
+                break;
+            
+            case 2:
+                displaySetAlarm();
+                break;
+        }
+    }
 } // void clickClock()
 
 
 // Клик против часовой стрелки
 void clickReverse()
 {
-    selectOption();
+    clickClock();
 } // void clickReverse()
 
 
 // Обработка выбранной опции
 void selectOption()
 {
-    D("SELECT [");
-    D(counter);
-    D_LN("]");
-    
-    display.clearDisplay();
-    
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(2*counter, counter);
-    display.print("26.04.19");
-    display.display();
+    if (!settings)
+    {
+        counter = 0;
+        displaySettings();
+        settings = true;
+        return;
+    }
+    else if (0 == window)
+    {
+        switch(counter)
+        {
+            case 0:
+                D_LN("wanna set time");
+                displaySetTime();
+                window = 1;
+                return;
+                
+            case 1:
+                D_LN("wanna set alarm");
+                displaySetAlarm();
+                window = 2;
+                return;
+                
+            case 2:
+                D_LN("wanna go home");
+                window = 0;
+                displayMain();
+                settings = false;
+                return;
+        }
+    }
+    else
+    {
+        switch(window)
+        {
+            case 1:
+                if (5 == counter)
+                {
+                    window = 0;
+                    displaySettings();
+                }
+                else
+                {
+                    timeChanging = true;
+                    timeSector = (byte) counter;
+                }
+                break;
+                
+            default:
+                D(window);
+                D(" ");
+                D_LN("UNDER CONSTRUCTION");
+        }
+    }
     
 } // void selectOption()
 
@@ -175,6 +256,9 @@ void displayMain()
     display.setCursor(83, 18);
     display.print("06:10");
 
+    display.setCursor(18, 14);
+    display.print("ASlab");
+
     display.setTextSize(2);
     display.setCursor(68, 0);
     display.print("23:41");
@@ -186,24 +270,113 @@ void displayMain()
     D_LN(lapsed);
 } // void displayAll()
 
-void blink()
+void displaySettings()
 {
-    display.setTextSize(2);
-    display.setCursor(blinkX, blinkY);
+    int selectOffset = 16;
+
+    counter = (3 + counter) % 3;
+    D("s");
+    D_LN(counter);
     
-    if (blinkIteration)
+    display.clearDisplay();
+    
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+
+    display.setCursor(selectOffset, counter * 10);
+    display.print(">");
+    display.setCursor(122 - selectOffset, counter * 10);
+    display.print("<");
+    
+    display.setCursor(32, 0);
+    display.print("set time");
+    
+    display.setCursor(32, 10);
+    display.print("set alarm");
+
+    display.setCursor(32, 20);
+    display.print("exit");
+    
+    display.display();
+} // void displaySettings()
+
+
+void displaySetTime()
+{
+    counter = (6 + counter) % 6;
+    D("t ");
+
+    display.clearDisplay();
+    
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+
+    int data[5] = {26, 04, 19, 23, 41};
+
+    for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++)
     {
-        display.setTextColor(BLACK);
+        String value;
+        if (data[i] < 10)
+        {
+            value = "0" + String(data[i]);
+        }
+        else
+        {
+            value = String(data[i]);
+        }
+
+        if (i == counter)
+        {
+            display.setTextColor(BLACK, WHITE);
+        }
+        else
+        {
+            display.setTextColor(WHITE);
+        }
+        
+        display.print(value);
+        
+        if (2 == i || 4 == i)
+        {
+            display.println();
+        }
+        else
+        {
+            display.setTextColor(WHITE);
+            if (i < 2)
+            {
+                display.print('.');
+            }
+            else
+            {
+                display.print(':');
+            }
+        }
+    }
+
+    if (5 == counter)
+    {
+        display.setTextColor(BLACK, WHITE);
     }
     else
     {
         display.setTextColor(WHITE);
     }
-    display.print(":");
+
+    display.setTextSize(1);
+    display.setCursor(80, 18);
+    display.print(" exit ");
+    
     display.display();
     
-    blinkIteration = !blinkIteration;
-} // void blink()
+} // void displaySetTime()
+
+
+void displaySetAlarm()
+{
+    
+} // void displaySetAlarm()
 
 void setup() 
 {
@@ -220,6 +393,9 @@ void setup()
     //EEPROM.get(State, 0);
 
     //check();
+    prevSW = digitalRead(ENCODER_SW);
+    window = 0;
+    
     displayMain();
 } // void setup()
 
@@ -229,13 +405,16 @@ void loop()
     checkEncoder();
 
     // Обработка нажатия кнопки энкодера
-    bool encoderBtn = digitalRead(ENCODER_SW);
-    if (!encoderBtn)
+    bool encoderSW = digitalRead(ENCODER_SW);
+    if (!encoderSW && prevSW)
     {
+        D_LN("click");
         // Обработка выбранной опции
         selectOption();
     }
 
+    prevSW = encoderSW;
+    delay(1);
 }
 
 
